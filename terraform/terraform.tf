30fa46e72aea28da37a5cea3a2f84e6681412b59
MohammedBentimzal <bentimzalmohammed@gmail.com>
2026-07-17 12:41:55 +0100

    provisiondeployment envirment for backend app on cloud aws

---
+++ terraform/terraform.tf
@@ -0,0 +1,10 @@
+# configure terraform , mention cloud provider aws , its version , version of terraform 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  required_version = ">= 1.2"
+}
